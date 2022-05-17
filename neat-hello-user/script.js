8e8aa2a05b69f76ba465f872bfc8d79bc1d5992c
Atikingi <n.igumentsev@gmail.com>
2022-05-17 20:54:52 +0300

    Add logic

---
+++ neat-hello-user/script.js
@@ -0,0 +1,15 @@
+const buttons = document.querySelectorAll('.hello-button');
+
+const helloBox = (event) => {
+    const target = event.target;
+    const targetParent = target.parentElement;
+  
+    target.remove();
+    const newElem = document.createElement('h1');
+    newElem.textContent = 'Привет, пользователь!';
+    targetParent.appendChild(newElem);
+}
+
+buttons.forEach ((button) => {
+    button.onclick = helloBox;
+})
