41e94104e89510cb9fde76decea53d882f699634
Atikingi <101656726+Atikingi@users.noreply.github.com>
2022-05-24 21:20:31 +0300

    Add logic

---
+++ accept-to-mailing/script.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const form = document.querySelector('.form');
+  const checkboxWrapper = document.querySelector('.form__accept');
+  const checkbox = document.querySelector('.form__accept-check');
+  const errorMessage = document.querySelector('.form__error-message');
+
+  form.addEventListener('submit', (event) => {
+    if (checkbox.checked === false) {
+      event.preventDefault();
+      checkboxWrapper.classList.add('form__error');
+      errorMessage.classList.remove('form__error-message__hidden');
+    }
+  });
+
+  form.addEventListener('change', () => {
+    checkboxWrapper.classList.remove('form__error');
+    errorMessage.classList.add('form__error-message__hidden');
+  });
+});
