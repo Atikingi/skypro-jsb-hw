36d157b7d61c92309a24b6c389c922b0f3c9f987
Atikingi <101656726+Atikingi@users.noreply.github.com>
2022-05-25 12:12:41 +0300

    Update come-to-city.js

--- go-to-magadan/come-to-city.js
+++ go-to-magadan/come-to-city.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     const cityName = document.querySelector('.city-name');
-    let params = (new URL(document.location)).searchParams;
+    const params = (new URL(document.location)).searchParams;
     const cityValue = params.get('city');
 
     cityName.textContent = `Еду в ${cities[cityValue]}`
