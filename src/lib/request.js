6593a3c989844202671b5640c7c6ff8755fd94f2
Atikingi <n.igumentsev@gmail.com>
2022-06-14 20:58:54 +0300

    add logic

---
+++ src/lib/request.js
@@ -0,0 +1,34 @@
+const noOperation = () => {};
+
+function request ({
+    method = 'GET',
+    url,
+    type = 'json',
+    onSuccess = noOperation,
+    onError = noOperation,
+  }){
+    const req = new XMLHttpRequest();
+
+    req.open(method, url);
+    req.responseType = type;
+
+    req.onload = (event) => {
+      const { target } = event;
+
+      if (target.status !== 200) {
+        onError(target.statusText);
+
+        return;
+      }
+
+      onSuccess(target.response);
+    };
+
+    req.onerror = () => {
+      onError('Непредвиденная ошибка. Попробуйте позже.');
+
+      return;
+    };
+
+    req.send();
+  };
